93a19ab5f4a2f0366dd32fe77fa3391d42c4a8eb
Madziorex <92018274+Madziorex@users.noreply.github.com>
2023-10-27 04:04:33 +0200

    pr_dom_madzi

---
+++ prdom_c#/Program.cs
@@ -0,0 +1,60 @@
+﻿int[] a = { 1, 2, 3 };
+int[] b = { 4, 5, 6 };
+List<int> wynik = new List<int>();
+List<int> wynikRev = new List<int>();
+bool plus;
+plus = false;
+
+Array.Reverse(a);
+Array.Reverse(b);
+
+if (a.Length < b.Length)
+{
+    int[] b2 = b;
+    b = a;
+    a = b2;
+}
+
+for(int i = 0; i < b.Length; i++)
+{
+    if ((a[i] + b[i]) < 10 && plus == false)
+    {
+        wynik.Add(a[i] + b[i]);
+    }
+    else if(plus == true)
+    {
+        if((a[i] + b[i] + 1) > 9)
+        {
+            wynik.Add((a[i] + b[i] + 1)%10);
+        }
+        else
+        {
+            wynik.Add(a[i] + b[i] + 1);
+            plus = false;
+        }
+    }
+    else
+    {
+        plus = true;
+        wynik.Add((a[i] + b[i] + 1) % 10);
+        wynik.Add(1);
+    }
+}
+
+if (a.Length > b.Length)
+{
+    for (int i = b.Length; i < a.Length; i++)
+    {
+        wynik.Add(a[i]);
+    }
+}
+
+for (int i = wynik.Count - 1; i >= 0; i--)
+{
+    wynikRev.Add(wynik[i]);
+}
+
+foreach (int i in wynikRev)
+{
+    Console.WriteLine(i);
+}
---
+++ prdom_selekcja/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] a = { 3, 2, 4, 3, 1, 2, 0 };
+int temp = 0;
+int temp2 = 0;
+int temp3 = 0;
+
+
+for (int i = 0; i < a.Length; i++)
+{
+    temp = a[i];
+    for (int j = i; j < a.Length; j++)
+    {
+        if (a[j] < temp)
+        {
+            temp = a[j];
+            temp2 = j;
+        }
+    }
+    temp3 = a[i];
+    a[i] = temp;
+    a[temp2] = temp3;
+}
+
+foreach (int i in a)
+{
+    Console.WriteLine(i);
+}
