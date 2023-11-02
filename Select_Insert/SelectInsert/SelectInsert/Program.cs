f3b6ba5aecf599536d0b8a7aca5e59979ca8d817
StanleyN0vak <darkmarek2@gmail.com>
2023-11-02 23:01:22 +0100

    Select_Insert

---
+++ Select_Insert/SelectInsert/SelectInsert/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] tab = { 6, 5, 3, 1, 8, 7, 2, 4 }; int temp1 = 0, x = 0, j = 0;
+
+for (int i = 1; i < tab.Length; i++)
+{
+    j = i - 1;
+    x = i;
+    while (j >= 0 && tab[x] < tab[j])
+    {
+        temp1 = tab[x];
+        tab[x] = tab[j];
+        tab[j] = temp1;
+        j--; x--;
+    }
+}
+
+foreach (int i in tab)
+{
+    Console.WriteLine(i);
+}
